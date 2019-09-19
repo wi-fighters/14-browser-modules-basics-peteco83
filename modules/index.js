bbbf4d008954192b4a8515c4153c0369a4b4353b
Fran Borg <francesca.bte@outlook.com>
2019-09-19 15:11:41 +0200

    Call functions

---
+++ modules/index.js
@@ -0,0 +1,8 @@
+import {scrollWindow} from "/modules/scroll.js";
+import {clickImages} from "/modules/click.js";
+import {hoverHeader} from "/modules/hover.js";
+
+
+scrollWindow();
+clickImages();
+hoverHeader();
